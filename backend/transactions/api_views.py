f4127f4af4b5bd56c5e8a9132e27a07baf2a2ae1
Muhammad Diponegoro <juhyuko@gmail.com>
2025-08-02 02:45:21 +0700

    feat: Add /api/whoami/ endpoint to get authenticated user data

--- backend/transactions/api_views.py
+++ backend/transactions/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Category, Wallet, Payee, Transaction, Budget, FinancialGoal, Debt, Transfer
