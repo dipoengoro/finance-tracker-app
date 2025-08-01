59ee196a346a94b02bf0d8774f706177b89c1ab6
Muhammad Diponegoro <juhyuko@gmail.com>
2025-08-02 01:25:52 +0700

    fix: Add allauth middleware

--- backend/finance_tracker_app/settings.py
+++ backend/finance_tracker_app/settings.py
@@ -81,7 +81,8 @@ MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'whitenoise.middl
               'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
               'django.contrib.auth.middleware.AuthenticationMiddleware',
               'django.contrib.messages.middleware.MessageMiddleware',
-              'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
+              'django.middleware.clickjacking.XFrameOptionsMiddleware',
+              'allauth.account.middleware.AccountMiddleware', ]
 
 ROOT_URLCONF = 'finance_tracker_app.urls'
 
